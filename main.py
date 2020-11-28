2766f89f3d236da7fddab3fa3202475031d40f4c
confar <tentigers@mail.ru>
2020-11-28 12:23:14 +0300

    clean requirements due to limit changeon heroku

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI, File
-from pydantic import BaseModel
 
 from classifier import Classifier
 
 resnet = Classifier()
 app = FastAPI()
-
-
-class PredictionResult(BaseModel):
-    prediction: str
-    probability: float
 
 
 @app.get('/')
@@ -20,4 +14,5 @@ def healthcheck():
 @app.post('/files/')
 async def recognize_flower(file: bytes = File(...)):
     pred, pred_idx, probs = resnet.learner.predict(file)
-    return PredictionResult(prediction=pred, probability=probs[pred_idx])
+    return {'prediction': pred,
+            'probability': float(probs[pred_idx])}
