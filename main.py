2fb7ec7da41524e3e794cbfc624409cf409a4eef
confar <tentigers@mail.ru>
2020-11-28 14:30:09 +0300

    change gunicorn for unicorn

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI, File
 
 from classifier import Classifier
 
-resnet = Classifier()
 app = FastAPI()
+app.model = Classifier()
 
 
 @app.get('/')
 def healthcheck():
-    return {'Hello': 'World'}
+    return {'alive': True}
 
 
 @app.post('/files/')
 async def recognize_flower(file: bytes = File(...)):
-    pred, pred_idx, probs = resnet.learner.predict(file)
+    pred, pred_idx, probs = app.model.learner.predict(file)
     return {'prediction': pred,
             'probability': float(probs[pred_idx])}
